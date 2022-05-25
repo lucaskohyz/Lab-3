a360d5f8b1cb7d021b544ced29de398f0f200b15
Lucas koh <{lucaskohyz@gmail.com}>
2022-05-25 21:20:20 +0800

    push

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -5,17 +5,18 @@ print("Test_Lab3")
 
 def test_bubble_sort_ascending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [11, 12, 22, 25, 34, 64, 90]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 10,20,30]
+    test_arr = [10,11, 12, 20,22, 25, 30,34, 64, 90]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
 
     assert (result == test_arr)
 
+
 def test_bubble_sort_descending():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
-    test_arr = [90, 64, 34, 25, 22, 12, 11]
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 10, 20, 30]
+    test_arr = [90, 64, 34,30, 25, 22,20, 12, 11, 10]
 
     result = Lab3.bubble_sort(input_arr, Lab3.SORT_DESCENDING)
 
@@ -23,8 +24,13 @@ def test_bubble_sort_descending():
 
 def test_bubble_sort_invalid():
     result = []
-    input_arr = [64, 34, 25, 12, 22, 11, 90]
+    input_arr = [64, 34, 25, 12, 22, 11, 90,10,20,30]
 
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+def checkifnumbermorethan_10():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90,10,20,30,40 ]
+    result = Lab3.bubble.sort(input_arr,4)
+    assert (result ==1)
